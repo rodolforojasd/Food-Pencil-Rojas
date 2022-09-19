c3d240f95a77e32e76ac5e83026be252fc474af1
Rodolfo Rojas <rodolfo.rojas.d@gmail.com>
2022-09-19 19:27:14 -0300

    implmentacion de SASS, sintaxis de SASS, excepciones de Bootstrap

--- scripts/codes.js
+++ scripts/codes.js
@@ -4,10 +4,10 @@ window.onscroll=function()
 function scrollFunction(){
 	if (document.body.scrollTop > 10) ||
 	document.documentElement.scrollTop > 10 {
-		document.getElementById("header-area-logo").style.display="none";
+		document.getElementById("logo-big").style.display="none";
 		document.getElementById("logo-small").style.display="inline-block";
 	} else {
-		document.getElementById("header-area-logo").style.display="block";
+		document.getElementById("logo-big").style.display="inline-block";
 		document.getElementById("logo-small").style.display="none";	
 	}
 } 
